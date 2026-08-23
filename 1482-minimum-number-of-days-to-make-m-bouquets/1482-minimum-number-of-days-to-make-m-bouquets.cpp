8374f0d4a68bf8dc65a4620fb75bccb40f60646b
Prakhar Chandra <prakharchandra09@gmail.com>
2026-08-24 00:09:10 +0530

    Time: 20 ms (68.74%), Space: 70.4 MB (80.91%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool isPossible(vector<int>&bloomDay, long long m, long long k, int mid)
+    bool isPossible(vector<int>&bloomDay, long long m, long long k, long long mid)
     {   
         int count =0, noofbouquet=0;
        for(int i = 0; i<bloomDay.size(); i++)
